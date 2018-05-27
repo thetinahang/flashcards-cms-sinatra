decd11c5f679804d40e91ed77aefead2a6a7959d
thetinahang <thetinahang@gmail.com>
2018-05-27 12:33:13 -0700

    add new user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController 
 
 	get '/signup' do 
-		erb :'users/new'
+		if !session[:user_id]
+			erb :'users/new'
+		else 
+			redirect '/stacks'
 	end
 
 	post '/signup' do 
+		if params[:username] == "" || params[:password] == ""
+			redirect '/signup'
+		else 
+			@user = User.create(:username => params[:username], :password => params[:password])
+			session[:user_id] = @user.id 
+			redirect '/stacks'
+		end 
 	end 
 
 	get '/login' do 
